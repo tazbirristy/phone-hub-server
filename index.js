3ff70fd861f3b23005fe0180f619dccf1ebe1d45
tazbirristy <tazbirristyweb@gmail.com>
2022-11-29 02:37:13 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.suu4fk2.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", async (req, res) => {
+  res.send("Phone-hub server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Phone-hub is running on port ${port}`);
+});
